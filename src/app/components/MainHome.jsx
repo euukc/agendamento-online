5b09ebc7b0d56c93c76e26fd8822036656c4c85f
Kênia Caroline <keniacarolineferreira@gmail.com>
2024-06-06 00:45:05 -0300

    feat: Adiciona a segunda tela da aplicação

--- src/app/components/MainHome.jsx
+++ src/app/components/MainHome.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function MainHome(){
+    
+   
     return (
         <main>
             <div className="flex flex-col justify-center items-center text-center mb-[40px]">
@@ -9,7 +12,7 @@ export default function MainHome(){
             </div>
             <div className="flex justify-end md:justify-center pr-8">
                 <button className="border-2 border-transparent p-[10px] rounded-lg bg-[#FDCCC5] text-white w-[149px] font-extrabold">
-                    <Link href="/">Começar</Link>
+                    <Link href="/escolher-horario">Agendar</Link>
                 </button>
             </div>
         </main>
---
+++ src/pages/escolher-horario.jsx
@@ -0,0 +1,13 @@
+import HeaderChooseTime from "@/app/components/HeaderChooseTime"
+
+
+export default function escolherHorario(){
+    return (
+        <main className="w-full h-full justify-center items-center">
+            <header className="w-full h-full flex justify-center items-center">
+                <HeaderChooseTime />
+            </header>
+           
+        </main>
+    )
+}
