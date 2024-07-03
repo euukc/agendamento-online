b208f92cd564a2f9d46defe0e8608c946cf921eb
euukc <keniacarolineferreira@gmail.com>
2024-07-03 14:41:56 -0300

    incluindo validações para o usuário não deixar de escolher todas as informações para o agendamento

--- src/app/confirmar-agendamento/page.jsx
+++ src/app/confirmar-agendamento/page.jsx
@@ -11,7 +11,7 @@ export default function ConfirmarAgendamento() {
     const [time, setTime] = useState('');
     const [service, setService] = useState('');
 
-    useEffect(() => {      
+    useEffect(() => {
 
         const storedProfissional = localStorage.getItem('profissional');
         const storedDate = localStorage.getItem('date');
@@ -33,53 +33,58 @@ export default function ConfirmarAgendamento() {
     }, []);
 
     const agendamentoConfirmado = () => {
-        if (setProfissional === '' || setDate === '' || setTime === '' || setService === ''){
-            alert('Há informações não escolhidos. Por favor, verifique seu agendamento!')
-        } else
-        alert('Agendamento Confirmado!')
-        setProfissional('');
-        setDate('');
-        setTime('');
-        setService('');
+        if (!profissional || !date || !time || !service) {
+            alert('Há informações não escolhidas. Por favor, verifique seu agendamento!');
+        } else {
+            alert('Agendamento Confirmado!');
+            setProfissional('');
+            setDate('');
+            setTime('');
+            setService('');
+            localStorage.removeItem('profissional');
+            localStorage.removeItem('date');
+            localStorage.removeItem('time');
+            localStorage.removeItem('service');        
+        }
     }
 
-    return (
-        <main className="w-full h-min-screen flex flex-col justify-center mt-10">
-            <header className="text-center">
-                <div className="flex flex-col items-center gap-8 mx-[15px]">
-                    <h1 className="text-[19px]">
-                        <strong>MeTime</strong>
-                    </h1>
-                    <Image src="/icon-passagem4.png" width={100} height={50} alt="Icon" />
-                    <span className="text-[18px] md:text-[22px]">
-                        <strong>Confirme seu agendamento:</strong>
-                    </span>
-                </div>
-            </header>
+        return (
+            <main className="w-full h-min-screen flex flex-col justify-center mt-10">
+                <header className="text-center">
+                    <div className="flex flex-col items-center gap-8 mx-[15px]">
+                        <h1 className="text-[19px]">
+                            <strong>MeTime</strong>
+                        </h1>
+                        <Image src="/icon-passagem4.png" width={100} height={50} alt="Icon" />
+                        <span className="text-[18px] md:text-[22px]">
+                            <strong>Confirme seu agendamento:</strong>
+                        </span>
+                    </div>
+                </header>
 
-            <div className="w-full flex justify-center gap-10 md:gap-16 flex-wrap items-center mt-[40px] md:mt-[100px] text-[13px]">
-                <div className="w-ful flex flex-col ml-[10px] justify-center items-center text-[20px]">
-                    <span>
-                        <strong>Serviço escolhido: {service || 'Nenhum serviço selecionado'} </strong>
-                    </span>
-                    <span>Profissional escolhido: {profissional || 'Nenhum profissional selecionado'}</span>
+                <div className="w-full flex justify-center gap-10 md:gap-16 flex-wrap items-center mt-[40px] md:mt-[100px] text-[13px]">
+                    <div className="w-ful flex flex-col ml-[10px] justify-center items-center text-[20px]">
+                        <span>
+                            <strong>Serviço escolhido: {service || 'Nenhum serviço selecionado'} </strong>
+                        </span>
+                        <span>Profissional escolhido: {profissional || 'Nenhum profissional selecionado'}</span>
 
-                    <span>Dia e Hora: {date || 'Nenhuma data selecionada'} - {time || 'Nenhum horário selecionado'} </span>
+                        <span>Dia e Hora: {date || 'Nenhuma data selecionada'} - {time || 'Nenhum horário selecionado'} </span>
+                    </div>
                 </div>
-            </div>
-            <div className="flex justify-center items-center mx-[20px] gap-4 md:gap-[100px]">
-                <div className="w-[50%] flex justify-end mt-[40px] md:mt-[100px] mb-[50px]">
-                    <button className="border-2 border-transparent p-[5px] rounded-lg bg-[#867BEE] text-white w-[100px] md:w-[150px] md:h-[50px] text-[13px] font-extrabold">
-                        <Link href="/escolher-profissional">Voltar</Link>
-                    </button>
+                <div className="flex justify-center items-center mx-[20px] gap-4 md:gap-[100px]">
+                    <div className="w-[50%] flex justify-end mt-[40px] md:mt-[100px] mb-[50px]">
+                        <button className="border-2 border-transparent p-[5px] rounded-lg bg-[#867BEE] text-white w-[100px] md:w-[150px] md:h-[50px] text-[13px] font-extrabold">
+                            <Link href="/escolher-profissional">Voltar</Link>
+                        </button>
+                    </div>
+                    <div className="w-[50%] flex justify-start mt-[40px] md:mt-[100px] mb-[50px]">
+                        <button className="border-2 border-transparent p-[5px] rounded-lg bg-[#FDCCC5] text-white w-[100px] md:w-[150px] md:h-[50px] text-[13px] font-extrabold"
+                            onClick={agendamentoConfirmado}>
+                            Agendar
+                        </button>
+                    </div>
                 </div>
-                <div className="w-[50%] flex justify-start mt-[40px] md:mt-[100px] mb-[50px]">
-                    <button className="border-2 border-transparent p-[5px] rounded-lg bg-[#FDCCC5] text-white w-[100px] md:w-[150px] md:h-[50px] text-[13px] font-extrabold"
-                    onClick={agendamentoConfirmado}>
-                         Agendar
-                    </button>
-                </div>
-            </div>
-        </main>
-    );
-}
+            </main>
+        );
+    }
